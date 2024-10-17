fe3ed607f9f4149b418010b78a31e0ec063335b8
nguyenduyhaihauc <nguyenduyhaihauc@gmail.com>
2024-10-17 15:31:07 +0700

    Favorite Activity

---
+++ app/src/main/java/duyndph34554/fpoly/shop_app/Activity/FavoriteActivity.kt
@@ -0,0 +1,19 @@
+package duyndph34554.fpoly.shop_app.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import duyndph34554.fpoly.shop_app.R
+import duyndph34554.fpoly.shop_app.databinding.ActivityFavoriteBinding
+
+class FavoriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFavoriteBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFavoriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
